2d9501aa223a6c483479eafa64a5586f574ca77c
tgzgondoz <tgzgondozz@gmail.com>
2026-02-02 11:50:28 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(5000, () => console.log("Server running on port 5000"));
